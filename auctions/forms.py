01701cf506686d704e664d167324a6df3911fe36
Alicia Amador <alicia.amador.cabezali@gmail.com>
2023-09-16 21:05:14 +0200

    Change style with bootstrap

--- auctions/forms.py
+++ auctions/forms.py
@@ -2,8 +2,12 @@ from django import forms
 
 categories = [('1','Fashion'), ('2','Toys'), ('3','Electronics'), ('4','Home'),('5', 'Sport')]
 class NewProductForm(forms.Form):
-    title = forms.CharField(label= 'Title',widget=forms.TextInput(attrs={'name':'title'}))
-    description = forms.CharField(label= 'Description', widget=forms.Textarea(attrs={'name':'description', 'rows':3, 'cols':5}))
+    title = forms.CharField(label= 'Title',widget=forms.TextInput(attrs={'name':'title', 'class':'form-control'}))
+    description = forms.CharField(label= 'Description', widget=forms.Textarea(attrs={'name':'description', 'rows':3, 'cols':5, 'class':'form-control'}))
     initialPrice = forms.FloatField(label='Initial Bid')
     image = forms.URLField(label='Image')
-    category = forms.ChoiceField(label='Category', choices = categories)
+    category = forms.ChoiceField(label='Category', choices = categories, required=False)
+
+    initialPrice.widget.attrs.update({'class':'form-control'})
+    image.widget.attrs.update({'class':'form-control'})
+    category.widget.attrs.update({'class':'form-control'})
